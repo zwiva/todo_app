06540a8f25f7ef169b979ae7295013b6fa448a5c
Andrea Castro <ap.castrocastellon@gmail.com>
2019-06-04 11:13:25 -0400

    create created

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -5,5 +5,16 @@ class TodosController < ApplicationController
     def new
         @todo = Todo.new
     end
+    def create
+        @todo = Todo.new(todo_params)
+        @todo.save
+        redirect_to todos_path
+    end
 
+
+    private
+
+    def todo_params
+        params.require(:todo).permit(:description, :completed)
+    end
 end
